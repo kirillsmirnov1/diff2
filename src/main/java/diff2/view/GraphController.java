6eb712cb95e3fa5c76218167006f78017ec74d17
Kirill <thegaldren@gmail.com>
2019-01-27 10:53:24 +0300

    Добавление графиков функций

--- src/main/java/diff2/view/GraphController.java
+++ src/main/java/diff2/view/GraphController.java
@@ -2,8 +2,21 @@ package diff2.view;
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import org.la4j.Vector;
 
 public class GraphController {
     @FXML
     LineChart lineChart;
+
+    public void addSeries(String seriesName, Vector xSeries, Vector ySeries){
+        XYChart.Series series = new XYChart.Series();
+        series.setName(seriesName);
+
+        for(int i=0; i < xSeries.length(); ++i){
+            series.getData().add(new XYChart.Data(xSeries.get(i), ySeries.get(i)));
+        }
+
+        lineChart.getData().add(series);
+    }
 }
